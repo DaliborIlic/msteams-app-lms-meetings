9a61898838cf18c29df34da1f87d0a4afaef2b92
Dalibor Ilić <dalibor.ilic.dado@gmail.com>
2020-04-08 11:40:14 +0200

    Update msalApp.ts

--- src/auth/msalApp.ts
+++ src/auth/msalApp.ts
@@ -13,7 +13,7 @@ function isIE() {
 
 export const msalApp = new UserAgentApplication({
   auth: {
-    clientId: '80c4d6ec-82c5-44d4-a16d-cad66731f538', // TODO: move this into a cfg value from composition root
+    clientId: '9bcd8d6c-cd5d-4b75-a309-106347ebb6ec', // TODO: move this into a cfg value from composition root
     authority: 'https://login.microsoftonline.com/common',
     validateAuthority: true,
     postLogoutRedirectUri: 'http://localhost:3000',
